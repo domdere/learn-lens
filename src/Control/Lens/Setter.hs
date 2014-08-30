f163c8f33d7fd70077083521812ad4e48e26f574
Dom De Re <domdere@domdere.com>
2014-08-30 15:33:53 +1000

    Finished off the setters

--- src/Control/Lens/Setter.hs
+++ src/Control/Lens/Setter.hs
@@ -27,11 +27,12 @@ module Control.Lens.Setter (
 
 import LensPrelude
 import Data.Distributive
-import Data.Profunctor
 import Data.Functor.Identity
 
-import Control.Applicative ( Applicative )
+import Control.Applicative ( Applicative, pure )
 import Data.Traversable ( Traversable )
+
+infixr 4 .~
 
 -- | This is the specialisation of `Lens` on which the
 -- Setters are based, it should look reminiscent of
@@ -40,11 +41,15 @@ import Data.Traversable ( Traversable )
 type ASetter s t a b = (a -> Identity b) -> s -> Identity t
 
 -- |
--- This type class will empbody the properties of the Identity Functor
+-- This type class will embody the properties of the Identity Functor
 -- That we actually take advantage of in the Setters, so we can experiment
 -- in trying to apply them to other similar Functors.
 --
-class (Applicative f, Distributive f, Traversable f) -> Settable f where
+class (Applicative f, Distributive f, Traversable f) => Settable f where
+    untainted :: f a -> a
+
+instance Settable Identity where
+    untainted = runIdentity
 
 -- |
 -- This is the more general version that applies to all Functors that
@@ -54,3 +59,9 @@ class (Applicative f, Distributive f, Traversable f) -> Settable f where
 -- on, it will be useful to imagine `ASetter` in place of `Setter` when you see it in the types.
 --
 type Setter s t a b = forall f. (Settable f) => (a -> f b) -> s -> f t
+
+set :: ASetter s t a b -> b -> s -> t
+set l x y = untainted $ l ((const . pure) x) y
+
+(.~) :: ASetter s t a b -> b -> s -> t
+(.~) = set
